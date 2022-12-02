2b583a897986778e9d496824556f046dd3afaa32
macabretiresomedeer <119127437+macabretiresomedeer@users.noreply.github.com>
2022-12-02 11:53:52 +0800

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,15 +1,30 @@
 function submitted(){
-    document.getElementById('email').value = '';
-    document.getElementById('fname').value = '';
-    document.getElementById('lname').value = '';
-    document.getElementById('contact').value = '';
-    let submit = document.getElementById('submitted');
-    document.getElementById('submitted').style.opacity = '1';
-    setTimeout(function(){
-        submit.classList.toggle('fade');
-        document.getElementById('submitted').style.opacity = '0';
-    }, 2000);
-    setTimeout(function(){
-        submit.classList.toggle('fade');
-    }, 4000);
+    if(document.getElementById('fname').value == ""){
+        alert("pls enter firstname");
+    }
+    else if(document.getElementById('lname').value == ""){
+        alert("pls enter lastname");
+    }
+    else if(document.getElementById('email').value == ""){
+        alert("pls enter email");
+    }
+    else if(document.getElementById('contact').value == ""){
+        alert("pls enter contact");
+    }
+    else{
+        console.log("penis");
+        document.getElementById('email').value = "";
+        document.getElementById('fname').value = "";
+        document.getElementById('lname').value = "";
+        document.getElementById('contact').value = "";
+        let submit = document.getElementById('submitted');
+        document.getElementById('submitted').style.opacity = '1';
+        setTimeout(function(){
+            submit.classList.toggle('fade');
+            document.getElementById('submitted').style.opacity = '0';
+        }, 2000);
+        setTimeout(function(){
+            submit.classList.toggle('fade');
+        }, 4000);
+    }
 }
